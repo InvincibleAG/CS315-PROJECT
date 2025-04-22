0f1e5c992d83692230a1d97fc9320d73ab4b6e88
Manan Kumar <manankumar22@iitk.ac.in>
2025-04-22 20:43:00 +0530

    Update eventRoutes.js

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -5,13 +5,16 @@ const {
   getUserEvents,
   getAllEvents,
   getEventById,
-  updateEventStatus
+  updateEventStatus,
+  getEventsByStatus,
+  getConfirmedEvents
 } = require('../controllers/eventController');
 const { verifyToken } = require('../middleware/auth');
 
 // All routes require authentication
 router.use(verifyToken);
 
+router.get('/confirmed', getConfirmedEvents);
 // Get all events for the current user
 router.get('/user', getUserEvents);
 
@@ -20,6 +23,8 @@ router.get('/', getAllEvents);
 
 // Get a specific event
 router.get('/:id', getEventById);
+
+router.get('/status/:status', getEventsByStatus);
 
 // Create a new event
 router.post('/', createEvent);
