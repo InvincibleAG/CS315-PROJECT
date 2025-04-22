a7707dd3a519e5a5566c7175fe7a258cc54e2adf
Manan Kumar <manankumar22@iitk.ac.in>
2025-04-22 20:33:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,10 @@ import SignUp from './components/SignUp.jsx'
 import Events from './components/Events.jsx'
 import EventForm from './components/EventForm.jsx'
 import ProtectedRoute from './components/ProtectedRoute.jsx'
+import AdminRoute from './components/AdminRoute.jsx'
+import AdminEvents from './components/AdminEvents.jsx'
 import NavBar from './components/NavBar.jsx'
+import Calendar from './components/Calendar.jsx'
 
 export default function App() {
   return (
@@ -26,6 +29,16 @@ export default function App() {
             <ProtectedRoute>
               <EventForm />
             </ProtectedRoute>
+          } />
+          <Route path="/calendar" element={
+          <ProtectedRoute>
+            <Calendar />
+          </ProtectedRoute>
+          } />
+          <Route path="/admin/events" element={
+            <AdminRoute>
+              <AdminEvents />
+            </AdminRoute>
           } />
         </Routes>
       </div>
