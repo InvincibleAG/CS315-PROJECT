1ced36df39d87087a4bda1e66d47aca1c640bf98
Manan Kumar <manankumar22@iitk.ac.in>
2025-04-24 22:28:21 +0530

    Update hallRoutes.js

--- backend/routes/hallRoutes.js
+++ backend/routes/hallRoutes.js
@@ -3,13 +3,10 @@ const router = express.Router();
 const { getAllHalls, getHallAvailability } = require('../controllers/hallController');
 const { verifyToken } = require('../middleware/auth');
 
-// All routes require authentication
 router.use(verifyToken);
 
-// Get all halls
 router.get('/', getAllHalls);
 
-// Check hall availability
 router.get('/availability', getHallAvailability);
 
 module.exports = router;
